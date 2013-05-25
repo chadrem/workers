4ef9a65deaf52762531557180a4289fd13a36941
Chad Remesch <chad@remesch.com>
2013-05-25 18:51:22 -0400

    Log helpers now also look for @_logger (for Tribe actors).

--- lib/workers/helpers.rb
+++ lib/workers/helpers.rb
@@ -1,19 +1,19 @@
 module Workers
   module Helpers
     def log_debug(msg, e = nil)
-      @logger.debug(concat_e(msg, e))
+      (@_logger || @logger).debug(concat_e(msg, e))
     end
 
     def log_info(msg, e = nil)
-      @logger.info(concat_e(msg, e))
+      (@_logger || @logger).info(concat_e(msg, e))
     end
 
     def log_warn(msg, e = nil)
-      @logger.warn(concat_e(msg, e))
+      (@_logger || @logger).warn(concat_e(msg, e))
     end
 
     def log_error(msg, e = nil)
-      @logger.error(concat_e(msg, e))
+      (@_logger || @logger).error(concat_e(msg, e))
     end
 
     def concat_e(msg, e = nil)
