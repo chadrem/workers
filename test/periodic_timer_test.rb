1d1f8317c9c762ad0017d2bc9a2ac2dc0d0a8d6a
Chad Remesch <chad@remesch.com>
2015-07-17 12:13:31 -0400

    Timer tests.

--- test/periodic_timer_test.rb
+++ test/periodic_timer_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class PeriodicTimerTest < Minitest::Test
+  def test_basic_usage
+    counterA = 0
+    counterB = 0
+
+    timer = Workers::PeriodicTimer.new(0.01) do
+      counterA += 1
+    end
+
+    while counterB < 100
+      sleep 0.01
+      break if counterA >= 5
+      counterB += 1
+    end
+
+    assert(counterA >= 5)
+  ensure
+    timer.cancel
+  end
 end
--- test/timer_test.rb
+++ test/timer_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class TimerTest < Minitest::Test
+  def test_basic_usage
+    counterA = 0
+    counterB = 0
+
+    timer = Workers::Timer.new(0.01) do
+      counterA += 1
+    end
+
+    while counterB < 100
+      sleep 0.01
+      break if counterA  >= 1
+      counterB += 1
+    end
+
+    assert_equal(1, counterA)
+  ensure
+    timer.cancel
+  end
 end
