84428e9a9e6e224364e1e6da3a8d476dbe408e5a
Chad Remesch <chad@remesch.com>
2015-07-18 00:23:17 -0400

    Test bucket scheduler with timers.

--- test/periodic_timer_test.rb
+++ test/periodic_timer_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class PeriodicTimerTest < Minitest::Test
-  def test_basic_usage
+  def test_basic_usage_with_default_scheduler
+    perform_basic_test(Workers.scheduler)
+  end
+
+  def test_basic_usage_with_bucket_scheduler
+    scheduler = Workers::BucketScheduler.new
+    perform_basic_test(scheduler)
+  ensure
+    scheduler.dispose
+  end
+
+  private
+
+  def perform_basic_test(scheduler)
     counterA = 0
     counterB = 0
 
-    timer = Workers::PeriodicTimer.new(0.01) do
+    timer = Workers::PeriodicTimer.new(0.01, :scheduler => scheduler) do
       counterA += 1
     end
 
